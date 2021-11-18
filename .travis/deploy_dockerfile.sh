4404963f9766cf370abe8eaa5b783e909484e9ef
matsveyenak <61603703+matsveyenak@users.noreply.github.com>
2021-11-19 01:08:53 +0300

    dockerhub travis

--- .travis/deploy_dockerfile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-docker login -e aliaksei.matsveyenak@gmail.com -u matsveyenak -p slivko0022?
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-    TAG="latest"
-else
-    TAG="$TRAVIS_BRANCH"
-fi
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-docker push $TRAVIS_REPO_SLUG
